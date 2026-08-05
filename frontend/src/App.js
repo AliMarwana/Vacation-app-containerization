9ec7e1045f768255a1b8a9290fdc5301b3ac02e8
ali_marwana <ali.marwana@gabera.software>
2026-08-05 10:41:44 +0100

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+const API_BASE_URL = "http://68.183.124.41";
+const API_URL = process.env.REACT_APP_API_URL || API_BASE_URL;
 
 function App() {
   const [destinations, setDestinations] = useState([]);
